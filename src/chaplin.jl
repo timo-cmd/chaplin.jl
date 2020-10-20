4355808a168c9250f6d14372eba9c5b456de4f3b
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-10-20 15:50:34 +0200

    Update chaplin.jl

--- src/chaplin.jl
+++ src/chaplin.jl
@@ -1,1 +1,27 @@
 # tbd soon
+module Chaplin
+
+export
+ChaplinRequest,
+ChaplinServer,
+after,
+before,
+delete,
+get,
+patch,
+post,
+put,
+render_template,
+route,
+start,
+unroute
+
+include("log.jl")
+include("request.jl")
+include("route.jl")
+include("server.jl")
+include("template.jl")
+
+server = ChaplinServer(Nullable{Function}(), Nullable{Function}(), Array{route, 1}())
+
+end
